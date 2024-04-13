addbe1b80e6bfec90cddb7a6cc75950ce21a7ca0
Andrew Kostka <andrew.kostka@lookout.com>
2024-04-13 19:18:16 -0400

    remove test directory

--- test/mailers/previews/participant_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/participant_mailer
-class ParticipantMailerPreview < ActionMailer::Preview
-  def auction_summary_preview
-    ParticipantMailer.with(user: User.first).auction_summary("test@email.com")
-  end
-end
